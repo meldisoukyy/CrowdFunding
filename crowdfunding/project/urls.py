9ddab31a61c35edfa10dca0fa126523613ea6733
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2022-09-16 21:21:40 +0200

    Initialize TestCases

--- crowdfunding/project/urls.py
+++ crowdfunding/project/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views as vi
 
 urlpatterns = [
-    path('', vi.project, name='project'),
+    path('', vi.ProjectHome.as_view(), name='project_home'),
     path('create/', vi.CreateProject.as_view(), name='create_project'),
     path('update/', vi.update_project, name='update_project'),
     path('delete/<int:pk>', vi.delete_project, name='delete_project'),
--- crowdfunding/project/views.py
+++ crowdfunding/project/views.py
@@ -2,33 +2,20 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import reverse
 
+from django.views.generic import ListView
 from django.views.generic.edit import CreateView
 
 from .models import Project
 
 from .forms import ProjectForm
 
-
-def project(request):
-    all_projects = str(Project.objects.all())
-    return HttpResponse(all_projects)
-
-# def create_project(request):
-#     if request.POST:
-#         form = ProjectForm(request.POST, request.FILES)
-#         print(form)
-#         print(form.is_valid())
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponse('Success')
-#     context = {
-#         'form' : ProjectForm(),
-#         }
-#     return render(request, 'project/create.html', context = context)
+class ProjectHome(ListView):
+    model = Project
+    template_name: str = 'project/home.html'
+    context_object_name = 'all_projects'
 
 class CreateProject(CreateView):
     form_class = ProjectForm
-    # fields = '__all__'
     template_name = 'project/create.html'
 
     def get_success_url(self):
