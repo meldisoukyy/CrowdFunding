e7da4393e329942fec1f3e24557e4ac88fd88bdb
Mohamed Elsaeid Eldisouky <meldisoukyy@gmail.com>
2022-09-16 23:02:47 +0200

    Adding Categories

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'project.apps.ProjectConfig',
+    'categories.apps.CategoriesConfig',
 ]
 
 MIDDLEWARE = [
--- crowdfunding/project/models.py
+++ crowdfunding/project/models.py
@@ -1,13 +1,16 @@
 from datetime import date
-from pyexpat import model
 from django.db import models
+from categories.models import Category
 
 # Create your models here.
 class Project(models.Model):
     project_title = models.CharField(max_length=100, help_text='Project title should not exceed 100 character.')
-    # project_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    project_category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     project_details = models.TextField(null=True)
     # project_images = models.ImageField()
     project_total_target = models.IntegerField(null=True)
     project_start_date = models.DateField(null=True, blank=True, default=date.today())
     project_end_date = models.DateField(null=True, blank=True) 
+
+    def __str__(self):
+        return self.project_title
